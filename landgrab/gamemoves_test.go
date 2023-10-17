2aa66b03b74b8f681531d0b1b3fc9b6d17f6bc5e
Petrus Breedt <Petrus.Breedt@AdaptIT.com>
2023-10-17 17:33:31 -0500

    Fix unit tests

--- landgrab/gamemoves_test.go
+++ landgrab/gamemoves_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestSinglMenuDisplay(t *testing.T) {
 	menu := Menu{Category: "Category", Options: []Option{{Display: "Option 1", ActionKey: "1"}, {Display: "Option 2", ActionKey: "2"}}}
-	expect := "Category: Option 1 | Option 2\n"
+	expect := "Category       : Option 1 | Option 2\n"
 	got := makeMenuDisplay(menu)
 	if got != expect {
 		t.Errorf("expected:\n'%s', got:\n'%s'", expect, got)
@@ -53,7 +53,7 @@ func TestSingleAlphaMenuValid(t *testing.T) {
 func TestMultiMenuDisplay(t *testing.T) {
 	menu1 := Menu{Category: "Category1", Options: []Option{{Display: "Option 1.1", ActionKey: "1"}, {Display: "Option 1.2", ActionKey: "2"}}}
 	menu2 := Menu{Category: "Category2", Options: []Option{{Display: "Option 2.1", ActionKey: "1"}, {Display: "Option 2.2", ActionKey: "2"}}}
-	expect := "Category1: Option 1.1 | Option 1.2\nCategory2: Option 2.1 | Option 2.2\n"
+	expect := "Category1      : Option 1.1 | Option 1.2\nCategory2      : Option 2.1 | Option 2.2\n"
 	got := makeMenuDisplay(menu1, menu2)
 	if got != expect {
 		t.Errorf("expected:\n'%s', got:\n'%s'", expect, got)
